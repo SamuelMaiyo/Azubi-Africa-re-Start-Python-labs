bfbb6c5367205e1882db637f0511ada67bc226ac
Samule Maiyo <46650368+SamuelMaiyo@users.noreply.github.com>
2022-06-14 13:13:51 +0300

    First Commit

---
+++ collections.py
@@ -0,0 +1,31 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+
+myFavoriteDictionary = {
+    "Akua" : "Apple",
+    "Saanvi" : "Banana",
+    "Paulo" : "Pineapple"
+}
+
+print(myFavoriteDictionary)
+print(type(myFavoriteDictionary))
+
+print(myFavoriteDictionary["Akua"])
+print(myFavoriteDictionary["Saanvi"])
+print(myFavoriteDictionary["Paulo"])
---
+++ hello-world.py
@@ -0,0 +1,3 @@
+#print("Hello world")
+
+print("Python has three numeric types: int, float, and complex")
